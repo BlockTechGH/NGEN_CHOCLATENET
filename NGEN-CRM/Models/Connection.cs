f72d89d57e2e2dbbaa880c0f5585ffa865e4c45b
BlockTechGH <info@blocktechtechnology.com>
2024-01-02 19:57:59 +0530

    connection.cs

--- NGEN-CRM/Models/Connection.cs
+++ NGEN-CRM/Models/Connection.cs
@@ -7,15 +7,26 @@ namespace NGEN_CRM.Models
 {
     public class Connection
     {
-        //public string ConnectionString { get; } = @"Server=IT-DESKTOP\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=user;Password=password;MultipleActiveResultSets=true";//dev
-        //public string ConnectionString { get; } = @"Server=LAPTOP-EBN4GVUL\SQLEXPRESS;Database=3CX Competitive Wallboard;Trusted_Connection=True;MultipleActiveResultSets=true;timeout=3000";//dev
+        //wecare
+        //public string ConnectionString { get; } = @"Server=IT-DESKTOP\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=user;Password=password;MultipleActiveResultSets=true";
 
-        #if DEBUG
-                public string ConnectionString { get; } = @"Server=LAPTOP-EBN4GVUL\SQLEXPRESS;Database=3CX Competitive Wallboard;Trusted_Connection=True;MultipleActiveResultSets=true;timeout=3000"; //local
+        //developer
+        //public string ConnectionString { get; } = @"Server=LAPTOP-EBN4GVUL\SQLEXPRESS;Database=3CX Competitive Wallboard;Trusted_Connection=True;MultipleActiveResultSets=true;timeout=3000";
 
-        #else
-                public string ConnectionString { get; } = @"Server=IT-DESKTOP\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=user;Password=password;MultipleActiveResultSets=true"; //wecare
-        #endif
+        //chocolateness
+        //public string ConnectionString { get; } = @"Server=DC1\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=btadmin;Password=btadmin;MultipleActiveResultSets=true";
+
+#if DEBUG
+        //developer
+        public string ConnectionString { get; } = @"Server=LAPTOP-EBN4GVUL\SQLEXPRESS;Database=3CX Competitive Wallboard;Trusted_Connection=True;MultipleActiveResultSets=true;timeout=3000";
+
+#else
+        //wecare
+        public string ConnectionString { get; } = @"Server=IT-DESKTOP\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=user;Password=password;MultipleActiveResultSets=true";
+
+        //chocolateness
+        //public string ConnectionString { get; } = @"Server=IT-DESKTOP\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=user;Password=password;MultipleActiveResultSets=true";
+#endif
 
     }
 }

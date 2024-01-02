bb0725432f4b3c5858345c60e16c5ecea5321be2
BlockTechGH <info@blocktechtechnology.com>
2024-01-02 19:06:41 +0530

    web.config, connection.cs

--- NGEN-CRM/Models/Connection.cs
+++ NGEN-CRM/Models/Connection.cs
@@ -11,10 +11,10 @@ namespace NGEN_CRM.Models
         //public string ConnectionString { get; } = @"Server=LAPTOP-EBN4GVUL\SQLEXPRESS;Database=3CX Competitive Wallboard;Trusted_Connection=True;MultipleActiveResultSets=true;timeout=3000";//dev
 
         #if DEBUG
-                public string ConnectionString { get; } = @"Server=LAPTOP-EBN4GVUL\SQLEXPRESS;Database=3CX Competitive Wallboard;Trusted_Connection=True;MultipleActiveResultSets=true;timeout=3000";//dev
+                public string ConnectionString { get; } = @"Server=LAPTOP-EBN4GVUL\SQLEXPRESS;Database=3CX Competitive Wallboard;Trusted_Connection=True;MultipleActiveResultSets=true;timeout=3000"; //local
 
         #else
-                public string ConnectionString { get; } = @"Server=IT-DESKTOP\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=user;Password=password;MultipleActiveResultSets=true";//dev
+                public string ConnectionString { get; } = @"Server=IT-DESKTOP\SQLEXPRESS;Database=3CX Competitive Wallboard;User Id=user;Password=password;MultipleActiveResultSets=true"; //wecare
         #endif
 
     }
